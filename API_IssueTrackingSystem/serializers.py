417481b4d784fac507e1b0ff0293662e385f917e
TMee3 <theopsna@protonmail.com>
2023-08-14 04:08:52 +0200

    update

--- API_IssueTrackingSystem/serializers.py
+++ API_IssueTrackingSystem/serializers.py
@@ -2,48 +2,45 @@ from rest_framework import serializers
 from API_IssueTrackingSystem.models import Project, Contributor, Issue, Comment
 from django.contrib.auth.models import User
 
-# Serializer pour les commentaires
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
 
-# Serializer pour les problèmes (issues)
 class IssueSerializer(serializers.ModelSerializer):
-    project = serializers.PrimaryKeyRelatedField(read_only=True)  # Utilisation de PrimaryKeyRelatedField
-
     class Meta:
         model = Issue
         fields = ['id', 'title', 'project']
-        
-# Serializer pour les contributeurs
+        depth = 1
+
 class ContributorSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField()  # Utilisation de StringRelatedField pour l'affichage
-    project = serializers.PrimaryKeyRelatedField(read_only=True)
+    user = serializers.StringRelatedField()
 
     class Meta:
         model = Contributor
         fields = ['id', 'user', 'project', 'role']
 
-# Serializer pour les projets sans détails associés
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = ['id', 'title']
         extra_kwargs = {'author': {'read_only': True}}
 
-# Serializer pour les projets avec détails associés (problèmes et contributeurs)
-class ProjectSerializerFull(serializers.ModelSerializer):
+class ProjectSerializerFull(ProjectSerializer):
     issues = IssueSerializer(many=True, read_only=True)
     contributors = ContributorSerializer(many=True, read_only=True)
 
-    class Meta:
-        model = Project
-        fields = ['id', 'title', 'description', 'type', 'author', 'issues', 'contributors']
+    class Meta(ProjectSerializer.Meta):
+        fields = ProjectSerializer.Meta.fields + ['description', 'type', 'author', 'issues', 'contributors']
 
-# Serializer pour les utilisateurs
 class UsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
+    
+    def create(self, validated_data):
+        user = super(UsersSerializer, self).create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
--- API_IssueTrackingSystem/views.py
+++ API_IssueTrackingSystem/views.py
@@ -1,101 +1,82 @@
 from rest_framework import generics, status
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, BasePermission, SAFE_METHODS
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework import viewsets
 from django.contrib.auth.models import User
-
 from API_IssueTrackingSystem.models import Project, Contributor, Issue, Comment
-from API_IssueTrackingSystem.serializers import ProjectSerializer, ProjectSerializerFull,\
-    ContributorSerializer, IssueSerializer, CommentSerializer, UsersSerializer
+from API_IssueTrackingSystem.serializers import (
+    ProjectSerializer, ProjectSerializerFull, ContributorSerializer,
+    IssueSerializer, CommentSerializer, UsersSerializer
+)
 
-# Définition d'une classe pour la pagination standard
+# Permission personnalisée
+class IsAuthorOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
 
-# Vue pour les opérations CRUD liées aux projets
 class ProjectViewSet(ModelViewSet):
-    queryset = Project.objects.all()
     serializer_class = ProjectSerializer
     serializer_class_full = ProjectSerializerFull
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
     pagination_class = StandardResultsSetPagination
     filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ['title', 'description']
 
-    # Choix du sérialiseur en fonction de l'action de la vue
+    def get_queryset(self):
+        return Project.objects.filter(author=self.request.user)
+
     def get_serializer_class(self):
         if self.action == 'retrieve':
             return self.serializer_class_full
         return super().get_serializer_class()
 
-    # Création d'un projet avec un contributeur initial (auteur)
     def perform_create(self, serializer):
         project = serializer.save(author=self.request.user)
-        contributor = Contributor(user=self.request.user, project=project, role='auteur')
-        contributor.save()
-        return Response({"message": "Project created successfully", "data": serializer.data}, status=status.HTTP_201_CREATED)
+        Contributor.objects.create(user=self.request.user, project=project, role='auteur')
 
-# Vue pour les opérations CRUD liées aux contributeurs
 class ContributorViewSet(ModelViewSet):
     serializer_class = ContributorSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
 
-    # Filtrer les contributeurs en fonction du projet donné
     def get_queryset(self):
-        contributors = Contributor.objects.filter(project=self.kwargs.get('project_pk'))
-        return contributors
+        return Contributor.objects.filter(project__author=self.request.user)
 
-    # Récupérer la liste des contributeurs pour un projet
-    def list(self, request, *args, **kwargs):
-        contributors = self.get_queryset()
-        serializer = self.get_serializer(contributors, many=True)
-        return Response({"message": "Contributors retrieved successfully", "data": serializer.data})
-
-# Vue pour les opérations CRUD liées aux problèmes (issues)
 class IssueViewSet(ModelViewSet):
     serializer_class = IssueSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
     pagination_class = StandardResultsSetPagination
     filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ['title', 'description', 'tag', 'priority', 'status']
 
-    # Filtrer les problèmes en fonction du projet donné
     def get_queryset(self):
-        queryset = Issue.objects.filter(project=self.kwargs.get('project_pk'))
-        return queryset
+        return Issue.objects.filter(project__author=self.request.user)
 
-    # Récupérer la liste des problèmes pour un projet
-    def list(self, request, *args, **kwargs):
-        issues = self.get_queryset()
-        serializer = self.get_serializer(issues, many=True)
-        return Response({"message": "Issues retrieved successfully", "data": serializer.data})
-
-# Vue pour les opérations CRUD liées aux commentaires
 class CommentViewSet(ModelViewSet):
-    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
     pagination_class = StandardResultsSetPagination
     filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ['description']
 
-    # Récupérer la liste des commentaires pour un problème donné
-    def list(self, request, *args, **kwargs):
-        comments = self.queryset.filter(issue=self.kwargs.get('issue_pk'))
-        serializer = self.get_serializer(comments, many=True)
-        return Response({"message": "Comments retrieved successfully", "data": serializer.data})
+    def get_queryset(self):
+        return Comment.objects.filter(issue__project__author=self.request.user)
 
-# Vue pour la création d'utilisateurs
 class UserViewSet(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UsersSerializer
 
-    # Créer un utilisateur
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        return Response({"message": "User created successfully", "data": serializer.data}, status=status.HTTP_201_CREATED)
+class ProjectViewSet(viewsets.ModelViewSet):
+    serializer_class = ProjectSerializerFull  # Utilisation de la version complète pour inclure les problèmes et les contributeurs
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Project.objects.select_related('author').prefetch_related('issues', 'contributors__user')
