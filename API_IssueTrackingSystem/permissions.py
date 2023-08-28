8865c220ea1e577af0143c13dda1d3f1c37d3b92
TMee3 <theopsna@protonmail.com>
2023-08-28 15:23:39 +0200

    update

--- API_IssueTrackingSystem/permissions.py
+++ API_IssueTrackingSystem/permissions.py
@@ -1,39 +1,78 @@
 from rest_framework import permissions
-from rest_framework.permissions import IsAuthenticated, BasePermission, SAFE_METHODS
 from API_IssueTrackingSystem.models import Project, Issue, Comment, Contributor
 
-# Permission pour vérifier si l'utilisateur peut accéder aux projets sur lesquels il est contributeur
 class CanAccessContributorProjects(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return Project.objects.filter(contributors__user=request.user).exists()
-
-# Permission pour vérifier si l'utilisateur peut accéder aux problèmes sur lesquels il est contributeur
-class CanAccessContributorIssues(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return Issue.objects.filter(project__contributors__user=request.user).exists()
-
-# Permission pour vérifier si l'utilisateur peut accéder aux commentaires sur lesquels il est contributeur
-class CanAccessContributorComments(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return Comment.objects.filter(issue__project__contributors__user=request.user).exists()
-
-# Permission pour vérifier si l'utilisateur peut accéder aux contributeurs
-class CanAccessContributors(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return Contributor.objects.filter(user=request.user).exists()
-
-# Permission pour gérer les contributeurs (ajouter, supprimer) dans un projet
-class CanManageContributorsPermission(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if request.method == 'POST':
-            project_id = request.data.get('project')  # Assurez-vous d'adapter cela à votre logique
-            if Contributor.objects.filter(project_id=project_id, user=request.user, role='auteur').exists():
-                return True
-        return False
-
-# Permission pour autoriser uniquement l'auteur à modifier les objets
-class IsAuthorOrReadOnly(permissions.BasePermission):
+    """
+    Assurez-vous qu'un utilisateur peut accéder aux projets auxquels il contribue.
+    """
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:  # Méthodes sûres comme GET, HEAD, OPTIONS
+        return obj.contributors.filter(user=request.user).exists()
+
+class CanAccessContributorIssues(permissions.BasePermission):
+    """
+    Assurez-vous qu'un utilisateur peut accéder aux problèmes des projets auxquels il contribue.
+    """
+    def has_object_permission(self, request, view, obj):
+        return obj.project.contributors.filter(user=request.user).exists()
+
+class CanAccessContributorComments(permissions.BasePermission):
+    """
+    Assurez-vous qu'un utilisateur peut accéder aux commentaires des problèmes des projets auxquels il contribue.
+    """
+    def has_object_permission(self, request, view, obj):
+        return obj.issue.project.contributors.filter(user=request.user).exists()
+
+class CanAccessContributors(permissions.BasePermission):
+    """
+    Assurez-vous qu'un utilisateur peut accéder aux objets Contributor auxquels il est associé.
+    """
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
+
+class CanManageContributorsPermission(permissions.BasePermission):
+    """
+    Autorisez uniquement l'auteur d'un projet à ajouter ou supprimer des contributeurs.
+    """
+    def has_object_permission(self, request, view, obj):
+        return obj.project.author == request.user
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    """
+    Autorisez uniquement l'auteur à modifier des objets, tandis que d'autres ne peuvent que lire.
+    """
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
             return True
         return obj.author == request.user
+
+class IsOwner(permissions.BasePermission):
+    """
+    Assurez-vous que l'utilisateur est le propriétaire de l'objet.
+    """
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
+
+class IsContributor(permissions.BasePermission):
+    """
+    Assurez-vous que l'utilisateur est un contributeur au projet.
+    """
+    def has_object_permission(self, request, view, obj):
+        return obj.contributors.filter(user=request.user).exists()
+
+class CanCreateIssue(permissions.BasePermission):
+    """
+    Assurez-vous que seuls les contributeurs d'un projet peuvent créer des problèmes pour ce projet.
+    """
+    def has_permission(self, request, view):
+        project_id = request.data.get('project')
+        project = Project.objects.get(id=project_id)
+        return project.contributors.filter(user=request.user).exists()
+
+class CanCommentOnIssue(permissions.BasePermission):
+    """
+    Assurez-vous que seuls les contributeurs d'un projet peuvent commenter ses problèmes.
+    """
+    def has_permission(self, request, view):
+        issue_id = request.data.get('issue')
+        issue = Issue.objects.get(id=issue_id)
+        return issue.project.contributors.filter(user=request.user).exists()
--- API_IssueTrackingSystem/views.py
+++ API_IssueTrackingSystem/views.py
@@ -3,7 +3,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import CreateAPIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticated, BasePermission, SAFE_METHODS
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from django.contrib.auth.models import User
 from .models import Project, Contributor, Issue, Comment
@@ -14,93 +14,106 @@ from .serializers import (
 from .permissions import (
     CanAccessContributorProjects, CanAccessContributorIssues,
     CanAccessContributorComments, CanAccessContributors,
-    CanManageContributorsPermission, IsAuthorOrReadOnly
+    CanManageContributorsPermission, IsAuthorOrReadOnly,
+    IsOwner, IsContributor, CanCreateIssue, CanCommentOnIssue
 )
 
 class UserViewSet(generics.CreateAPIView):
+    """
+    Vue pour la création d'utilisateurs.
+    """
     queryset = User.objects.all()
     serializer_class = UsersSerializer
 
-# Pagination standard pour les vues
 class StandardResultsSetPagination(PageNumberPagination):
+    """
+    Pagination standard pour les vues.
+    """
     page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
 
-# Vue pour gérer les projets
 class ProjectViewSet(ModelViewSet):
+    """
+    Vue pour gérer les projets.
+    """
     serializer_class = ProjectSerializer
     serializer_class_full = ProjectSerializerFull
     pagination_class = StandardResultsSetPagination
     filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ['title', 'description']
-
-    # Utiliser IsAuthenticated pour exiger l'authentification pour toutes les actions
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, CanAccessContributorProjects, IsOwner]
 
     def get_queryset(self):
-        # Autoriser les contributeurs à accéder aux projets
-        if CanAccessContributorProjects().has_permission(self.request, self):
-            return Project.objects.filter(contributors__user=self.request.user)
-        else:
-            return Project.objects.filter(author=self.request.user)
+        """
+        Récupère uniquement les projets auxquels l'utilisateur est contributeur ou auteur.
+        """
+        return Project.objects.filter(contributors__user=self.request.user)
 
     def get_serializer_class(self):
+        """
+        Utilise un serializer différent pour l'action 'retrieve'.
+        """
         if self.action == 'retrieve':
             return self.serializer_class_full
         return super().get_serializer_class()
 
     def perform_create(self, serializer):
+        """
+        Crée un projet et ajoute l'utilisateur actuel en tant que contributeur.
+        """
         project = serializer.save(author=self.request.user)
         Contributor.objects.create(user=self.request.user, project=project, role='auteur')
 
-    # Appliquer la permission CanManageContributorsPermission pour ajouter/supprimer des contributeurs
-    def get_permissions(self):
-        if self.action == 'create':
-            permission_classes = [CanManageContributorsPermission]
-        else:
-            permission_classes = [IsAuthorOrReadOnly]
-        return [permission() for permission in permission_classes]
-
-# Vue pour gérer les contributeurs
 class ContributorViewSet(ModelViewSet):
+    """
+    Vue pour gérer les contributeurs.
+    """
     serializer_class = ContributorSerializer
-    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly, IsContributor]
 
     def get_queryset(self):
-        return Contributor.objects.filter(project__author=self.request.user)
+        """
+        Récupère uniquement les contributeurs des projets dont l'utilisateur est l'auteur ou contributeur.
+        """
+        return Contributor.objects.filter(project__contributors__user=self.request.user)
 
-# Vue pour gérer les problèmes
 class IssueViewSet(ModelViewSet):
+    """
+    Vue pour gérer les problèmes.
+    """
     serializer_class = IssueSerializer
-    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly, CanCreateIssue]
     pagination_class = StandardResultsSetPagination
     filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ['title', 'description', 'tag', 'priority', 'status']
 
-    # Appliquer la permission CanAccessContributorIssues pour accéder uniquement aux problèmes de projets auxquels l'utilisateur contribue
     def get_queryset(self):
+        """
+        Récupère uniquement les problèmes des projets auxquels l'utilisateur est contributeur.
+        """
         return Issue.objects.filter(project__contributors__user=self.request.user)
 
-# Vue pour gérer les commentaires
 class CommentViewSet(ModelViewSet):
+    """
+    Vue pour gérer les commentaires.
+    """
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly, CanCommentOnIssue]
     pagination_class = StandardResultsSetPagination
     filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ['description']
 
-    # Appliquer la permission CanAccessContributorComments pour accéder uniquement aux commentaires des problèmes de projets auxquels l'utilisateur contribue
     def get_queryset(self):
+        """
+        Récupère uniquement les commentaires des problèmes des projets auxquels l'utilisateur est contributeur.
+        """
         return Comment.objects.filter(issue__project__contributors__user=self.request.user)
 
-# Vue pour créer un utilisateur
-class UserCreateView(CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UsersSerializer
-
-# Vue pour accéder aux projets avec les problèmes et les contributeurs
 class FullProjectViewSet(viewsets.ModelViewSet):
+    """
+    Vue pour accéder aux projets avec les problèmes et les contributeurs.
+    """
     serializer_class = ProjectSerializerFull
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwner]
     queryset = Project.objects.select_related('author').prefetch_related('issues', 'contributors__user')
