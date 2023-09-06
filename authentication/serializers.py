ecd7de8554fa25234e149f377cf9404639ad7401
TMee3 <theopsna@protonmail.com>
2023-09-06 02:06:02 +0200

    new

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
+from django.core.exceptions import ValidationError, ValidationError
 from .models import Consent
 
 class UserSerializer(serializers.ModelSerializer):
@@ -24,5 +25,19 @@ class UserSerializer(serializers.ModelSerializer):
 
     def validate_email(self, value):
         if User.objects.filter(email=value).exists():
-            raise serializers.ValidationError("Cette adresse e-mail existe déjà.")
+            raise serializers.ValidationError("This email address already exists.")
+        
+        try:
+            from validate_email_address import validate_email
+            if not validate_email(value):
+                raise serializers.ValidationError("Please provide a valid email address.")
+        except ValidationError:
+            raise serializers.ValidationError("Please provide a valid email address.")
+        
         return value
+
+    def validate_consent_given(self, value):
+        if not value:
+            raise serializers.ValidationError("Vous devez donner votre consentement pour vous inscrire.")
+        return value
+    
