dfc2e389e389911acbac3eec40fe7f2fe5628a26
TMee3 <theopsna@protonmail.com>
2023-09-06 02:44:55 +0200

    Delete rgpd

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
 from .serializers import UserSerializer
 from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+
 
 class UserCreate(generics.CreateAPIView):
     """
@@ -9,3 +13,21 @@ class UserCreate(generics.CreateAPIView):
 
     queryset = User.objects.all()  # Récupération de tous les utilisateurs (non utilisé ici)
     serializer_class = UserSerializer  # Utilisation du sérialiseur pour la création d'utilisateurs
+
+    def perform_create(self, serializer):
+        """Sauvegarde de l'utilisateur nouvellement créé."""
+        serializer.save()
+
+    
+
+class UserDelete(generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request, *args, **kwargs):
+        user = request.user
+        if user.is_authenticated:  # Check if the user is authenticated
+            user.delete()
+            # reutrn a success message if the user is deleted
+            return Response({"message": "User deleted successfully"}, status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
--- softdesk/urls.py
+++ softdesk/urls.py
@@ -1,17 +1,32 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from authentication.views import UserCreate
+from authentication.views import UserCreate , UserDelete
 from project.views import (
     ProjectCreateAndList, ProjectDetail,
     ContributorCreateandList, ContributorDetail,
     IssueCreateAndList, IssueDetail,
     CommentCreateAndList, CommentDetail
 )
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API",
+        default_version='v1',
+        description="Your API description",
+        terms_of_service="https://www.yourapp.com/terms/",
+        contact=openapi.Contact(email="contact@yourapp.com"),
+        license=openapi.License(name="Your License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('api-auth/', include('rest_framework.urls')),
 
     # Authentification
     path('api/signup/', UserCreate.as_view(), name='signup'),
@@ -32,5 +47,12 @@ urlpatterns = [
 
     # Gestion des commentaires sur un problème
     path('api/project/<int:project_pk>/issue/<int:issue_pk>/comment/', CommentCreateAndList.as_view(), name='create_list_comment'),
-    path('api/project/<int:project_pk>/issue/<int:issue_pk>/comment/<int:comment_pk>/', CommentDetail.as_view(), name='create_list_comment')
+    path('api/project/<int:project_pk>/issue/<int:issue_pk>/comment/<int:comment_pk>/', CommentDetail.as_view(), name='create_list_comment'),
+
+    # Suprimer son compte
+    path('api/user/delete/', UserDelete.as_view(), name='delete_user'),
+
+    # Swagger documentation URLs
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
